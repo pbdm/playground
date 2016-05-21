d0154800a9b12f1821b8443bf6668b3b7c809ecc
PBDm <pbdm915@gmail.com>
2016-05-21 19:36:05 +0800

    mv react

--- react/js/components/Message.js
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react'
-
-class Message extends Component {
-  render() {
-    return <h3>Message {this.props.params.id}</h3>
-  }
-}
-
-export default Message 
--- react/js/components/NoMatch.js
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react'
-
-class NoMatch extends Component {
-  render() {
-    return <h3>NoMatch</h3>
-  }
-}
-
-export default NoMatch 
