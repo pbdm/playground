4b39fdf9997c1c8e7e39362ec4c4f9af54c9a70d
PBDm <pbdm915@gmail.com>
2016-06-13 19:03:05 +0800

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 console.log('app.js');
-//require('./components/promise.js')
-// require('./components/webworker.js')
 if (location.hash === '#inheritance') {
   require('./components/oo')
+} else if (location.hash === '#promise') {
+  require('./components/promise.js')
 }
