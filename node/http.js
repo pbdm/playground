8c33779562413bf9e0cc1fed7ca8079fc14bc0de
PBDm <pbdm915@gmail.com>
2018-05-30 16:29:33 +0800

    update

--- node/http.js
+++ node/http.js
@@ -2,8 +2,12 @@ var fs = require('fs');
 
 require('http')
   .createServer(function(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Credentials', true);
     console.log(req.headers['content-type']);
-    if (req.headers['content-type'].indexOf('multipart/form-data') !== -1) {
+    // 图片上传
+    if (req.headers['content-type'] && req.headers['content-type'].indexOf('multipart/form-data') !== -1) {
+      // https://www.cnblogs.com/axes/p/4308430.html
       var chunks = [];
       var size = 0;
       // 接收 body,  可能触发很多次
@@ -11,6 +15,7 @@ require('http')
         chunks.push(chunk);
         size += chunk.length;
       });
+      // 在所有数据接收完成后触发
       req.on('end', () => {
         var buffer = Buffer.concat(chunks, size);
         if (!size) {
@@ -18,9 +23,7 @@ require('http')
           res.end('');
           return;
         }
-
         var rems = [];
-
         //根据\r\n分离数据和报头
         for (var i = 0; i < buffer.length; i++) {
           var v = buffer[i];
@@ -29,21 +32,16 @@ require('http')
             rems.push(i);
           }
         }
-
         //图片信息
         var picmsg = buffer.slice(rems[0] + 2, rems[1]).toString();
         var path = picmsg.match(/filename=".*"/g)[0].split('"')[1];
-
         //图片数据
         var nbuf = buffer.slice(rems[3] + 2, rems[rems.length - 2]);
-
         fs.writeFileSync(path, nbuf);
         res.end('{"errMsg":"ok"}');
       });
     } else {
       // console.log(req.method);
-      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3002');
-      res.setHeader('Access-Control-Allow-Credentials', true);
       // res.setHeader(
       //   "Set-Cookie", 'a=b'
       // )
