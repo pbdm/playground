0e60be77b14125c4bc44c398c83254e7eebf55b4
PBDm <pbdm915@gmail.com>
2018-12-12 11:26:09 +0800

    webgl cube rotate

--- browser/webgl/draw.js
+++ browser/webgl/draw.js
@@ -36,9 +36,16 @@ const programInfo = {
   }
 };
 
-const buffers = initBuffers(gl);
-var then = 0;
+const buffers = {};
+// buffers.position = initPositionBuffers(gl);
+// buffers.color = initColorBuffers(gl);
 
+// for cube
+buffers.position = initCubePositionBuffers(gl);
+buffers.color = initCubeColorBuffers(gl);
+buffers.indices = initCubeIndicesBuffers(gl);
+
+var then = 0;
 // Draw the scene repeatedly
 function render(now) {
   now *= 0.001;  // convert to seconds
@@ -51,38 +58,6 @@ function render(now) {
 requestAnimationFrame(render);
 // drawScene(gl, programInfo, buffers);
 
-function initBuffers(gl) {
-  const positions = [
-    -1.0, 1.0,
-     1.0, 1.0,
-    -1.0, -1.0,
-     1.0, - 1.0
-  ];
-  // Create a buffer for the square's positions.
-  const positionBuffer = gl.createBuffer();
-  // Select the positionBuffer as the one to apply buffer
-  // operations to from here out.
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  // Now pass the list of positions into WebGL to build the
-  // shape. We do this by creating a Float32Array from the
-  // JavaScript array, then use it to fill the current buffer.
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-  
-  const colors = [
-    1.0, 1.0, 1.0, 1.0,    // 白色
-    1.0, 0.0, 0.0, 1.0,    // 红色
-    0.0, 1.0, 0.0, 1.0,    // 绿色
-    0.0, 0.0, 1.0, 1.0     // 蓝色
-  ];
-  const colorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-  return {
-    position: positionBuffer,
-    color: colorBuffer
-  };
-}
-
 function drawScene(gl, programInfo, buffers, deltaTime) {
   gl.clearColor(0.0, 0.0, 0.0, 1.0);  // Clear to black, fully opaque
   gl.clearDepth(1.0);                 // Clear everything
@@ -90,55 +65,37 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
   gl.depthFunc(gl.LEQUAL);            // Near things obscure far things
   // Clear the canvas before we start drawing on it.
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-  // Create a perspective matrix, a special matrix that is
-  // used to simulate the distortion of perspective in a camera.
-  // Our field of view is 45 degrees, with a width/height
-  // ratio that matches the display size of the canvas
-  // and we only want to see objects between 0.1 units
-  // and 100 units away rom the camera.
+
   // 设置45度的视图角度，并且宽高比设为 640/480（画布尺寸）。 指定在摄像机距离0.1到100单位长度的范围内，物体可见
   const fieldOfView =45 * Math.PI / 180;   // in radians
   const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
   const zNear = 0.1;
   const zFar = 100.0;
   const projectionMatrix = mat4.create();
-  // note: glmatrix.js always has the first argument
-  // as the destination to receive the result.
-  mat4.perspective(projectionMatrix,
-                   fieldOfView,
-                   aspect,
-                   zNear,
-                   zFar);
-  // Set the drawing position to the "identity" point, which is
-  // the center of the scene.
-  const modelViewMatrix = mat4.create();
-  // Now move the drawing position a bit to where we want to
-  // start drawing the square.
+  mat4.perspective(projectionMatrix, fieldOfView, aspect, zNear, zFar);
   // 把物体放在距离摄像机6个单位的的位置
-  mat4.translate(modelViewMatrix,     // destination matrix
-                 modelViewMatrix,     // matrix to translate
-                 [-0.0, 0.0, -6.0]);  // amount to translate
-  mat4.rotate(modelViewMatrix,  // destination matrix
-              modelViewMatrix,  // matrix to rotate
-              squareRotation,   // amount to rotate in radians
-              [0, 0, 1]);       // axis to rotate around
+  const modelViewMatrix = mat4.create();
+  mat4.translate(modelViewMatrix, modelViewMatrix, [-0.0, 0.0, -6.0]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, squareRotation, [0, 0, 1]);
+  mat4.rotate(modelViewMatrix, modelViewMatrix, squareRotation * .7, [0, 1, 0]);
+
   // Tell WebGL how to pull out the positions from the position
   // buffer into the vertexPosition attribute.
   {
-    const numComponents = 2;  // pull out 2 values per iteration
+    const numComponents = 3;  // pull out 2 values per iteration
     const type = gl.FLOAT;    // the data in the buffer is 32bit floats
     const normalize = false;  // don't normalize
     const stride = 0;         // how many bytes to get from one set of values to the next
-                              // 0 = use type and numComponents above
     const offset = 0;         // how many bytes inside the buffer to start from
     gl.bindBuffer(gl.ARRAY_BUFFER, buffers.position);
     gl.vertexAttribPointer(
-        programInfo.attribLocations.vertexPosition,
-        numComponents,
-        type,
-        normalize,
-        stride,
-        offset);
+      programInfo.attribLocations.vertexPosition,
+      numComponents,
+      type,
+      normalize,
+      stride,
+      offset
+    );
     gl.enableVertexAttribArray(programInfo.attribLocations.vertexPosition);
   }
 
@@ -150,24 +107,19 @@ function drawScene(gl, programInfo, buffers, deltaTime) {
     gl.FLOAT, 
     false, 
     0, 
-  0);
+    0
+  );
   gl.enableVertexAttribArray(programInfo.attribLocations.vertexColor);
+
+  // TODO 这里为什么在渲染立方体的时候可有可无?!
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
 
   // Tell WebGL to use our program when drawing
   gl.useProgram(programInfo.program);
-  // Set the shader uniforms
-  gl.uniformMatrix4fv(
-      programInfo.uniformLocations.projectionMatrix,
-      false,
-      projectionMatrix);
-  gl.uniformMatrix4fv(
-      programInfo.uniformLocations.modelViewMatrix,
-      false,
-      modelViewMatrix);
-  {
-    const offset = 0;
-    const vertexCount = 4;
-    gl.drawArrays(gl.TRIANGLE_STRIP, offset, vertexCount);
-  }
+  gl.uniformMatrix4fv(programInfo.uniformLocations.projectionMatrix, false, projectionMatrix);
+  gl.uniformMatrix4fv(programInfo.uniformLocations.modelViewMatrix, false, modelViewMatrix);
+  
+  // gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0);
   squareRotation += deltaTime;
 }
