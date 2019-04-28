d3c08d198cdaf56da331a13fde2e629cdb748e1b
PBDm <pbdm915@gmail.com>
2019-04-28 11:38:20 +0800

    update

--- node/express-handler-error.js
+++ node/express-handler-error.js
@@ -1,29 +1,19 @@
+// nodejs的异步和错误处理 https://cnodejs.org/topic/56aad41a26d02fc6626bb369
+// https://jysperm.me/2016/10/nodejs-error-handling/
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res, next) {
-  console.log('here')
-  next();
+// 个人推荐方式
+app.get('/recommend', function (req, res, next) {
+  try {
+    JSON.parse('dd');
+  } catch (e) {
+    next(e)
+  }
 });
 
-app.get('/', function (req, res) {
-  res.send('root');
-});
-
-// nodejs的异步和错误处理 https://cnodejs.org/topic/56aad41a26d02fc6626bb369
 app.get('/error', function (req, res) {
   throw new Error('an error')
-});
-
-
-app.get('/cacheerror', function (req, res) {
-  try {
-    throw new Error('an cached error')
-  } catch (e) {
-    res.json({
-      catchMessage: e.message
-    });
-  }
 });
 
 // 走 promise 内的 catch
@@ -70,10 +60,15 @@ app.get('/timeouterror', function (req, res) {
   }, 1000)
 });
 
+// error handler
 app.use(function(err, req, res, next) {
-  res.json({
-    message: err.message
-  });
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send(err.message || 'error');
 });
 
 app.listen(5000, function () {
@@ -81,8 +76,7 @@ app.listen(5000, function () {
 });
 
 // 并不推荐, 实际上有一些比较激进的人士认为程序一旦出现事先没有预料到的错误，就应该立刻崩溃，以免造成进一步的不可控状态，也为了提起开发人员足够的重视
-// https://jysperm.me/2016/10/nodejs-error-handling/
-process.on('uncaughtException', (err) => {
-  console.log('uncaughtException')
-  console.error(err);
-});
+// process.on('uncaughtException', (err) => {
+//   console.log('uncaughtException')
+//   console.error(err);
+// });
