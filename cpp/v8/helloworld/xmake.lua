4cddaa9fcd2a9567bc885a2828f79f27492e8d01
PBDm <pbdm915@gmail.com>
2020-03-18 17:27:30 +0800

    update v8

--- cpp/v8/helloworld/xmake.lua
+++
@@ -1,7 +0,0 @@
-target("hello")
-	set_kind("binary")
-	set_languages("c++11")
-	add_includedirs("/Users/bopeng/c/tools/v8/include")
-	add_linkdirs("/Users/bopeng/c/tools/v8/out.gn/x64.release.sample/obj")
-	add_links("v8_monolith")
-	add_files("*.cc") 
